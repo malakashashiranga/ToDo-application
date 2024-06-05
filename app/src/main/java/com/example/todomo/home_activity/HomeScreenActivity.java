7d746156b507af3abdd17d69914c9f8ae7cf0d3c
malakashashiranga <malakashashiranga115@gmail.com>
2024-06-05 06:54:34 +0530

    insert home screen layout

---
+++ app/src/main/java/com/example/todomo/home_activity/HomeScreenActivity.java
@@ -0,0 +1,16 @@
+package com.example.todomo.home_activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.todomo.R;
+
+public class HomeScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+    }
+}
