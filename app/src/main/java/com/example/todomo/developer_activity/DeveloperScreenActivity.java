554490c7831436a71d7dbbad763cbc3104c2751e
malakashashiranga <malakashashiranga115@gmail.com>
2024-06-06 14:12:59 +0530

    insert developer info screen layout

---
+++ app/src/main/java/com/example/todomo/developer_activity/DeveloperScreenActivity.java
@@ -0,0 +1,16 @@
+package com.example.todomo.developer_activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.todomo.R;
+
+public class DeveloperScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_developer_screen);
+    }
+}
