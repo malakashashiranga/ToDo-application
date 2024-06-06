352cdc772eed1085c27087b9205c97eb31f7cd8b
malakashashiranga <malakashashiranga115@gmail.com>
2024-06-06 19:48:20 +0530

    Set screen moving part

--- app/src/main/java/com/example/todomo/splash_activity/SplashScreenActivity.java
+++ app/src/main/java/com/example/todomo/splash_activity/SplashScreenActivity.java
@@ -2,9 +2,7 @@ package com.example.todomo.splash_activity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.example.todomo.R;
 
 @SuppressLint("CustomSplashScreen")
@@ -14,5 +12,7 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        SplashScreenActionHandler.splashDelayedNavigation(this, 3000);
     }
 }
