c5637be1833724beff1a34e4660bf302fa8a55bb
malakashashiranga <malakashashiranga115@gmail.com>
2024-06-09 10:22:24 +0530

    add set password screen actions

--- app/src/main/java/com/example/todomo/sign_up_activity/set_password_activity/SetPasswordScreenActivity.java
+++ app/src/main/java/com/example/todomo/sign_up_activity/set_password_activity/SetPasswordScreenActivity.java
@@ -2,12 +2,13 @@ package com.example.todomo.sign_up_activity.set_password_activity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ProgressBar;
 
+import androidx.activity.OnBackPressedCallback;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.todomo.R;
-
 
 public class SetPasswordScreenActivity extends AppCompatActivity {
 
@@ -17,7 +18,19 @@ public class SetPasswordScreenActivity extends AppCompatActivity {
         setContentView(R.layout.activity_set_password_screen);
 
         ProgressBar progressBar = findViewById(R.id.PasswordSpinnerBar);
-
         progressBar.setVisibility(View.INVISIBLE);
+
+        Button finishButton = findViewById(R.id.FinishButton);
+
+        SetPasswordScreenActionHandler.initializeViews(finishButton, progressBar);
+        SetPasswordScreenActionHandler.handleFinishButtonClick(this);
+
+        OnBackPressedCallback callback = new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                SetPasswordScreenActionHandler.handleBackButtonClick(SetPasswordScreenActivity.this);
+            }
+        };
+        getOnBackPressedDispatcher().addCallback(this, callback);
     }
 }
