7d510cfb67ea27fe4771bf4a132dad384be5804b
malakashashiranga <malakashashiranga115@gmail.com>
2024-06-09 23:35:14 +0530

    add home screen actions

--- app/src/main/java/com/example/todomo/home_activity/HomeScreenActivity.java
+++ app/src/main/java/com/example/todomo/home_activity/HomeScreenActivity.java
@@ -1,6 +1,10 @@
 package com.example.todomo.home_activity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,5 +16,19 @@ public class HomeScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+
+        ImageButton profileImageButton = findViewById(R.id.ProfileImageButton);
+
+        Button createButton = findViewById(R.id.CreateButton);
+
+        LinearLayout scrollLayout = findViewById(R.id.ScrollLayout);
+
+        TextView eventAlert = findViewById(R.id.EventAlert);
+
+        HomeScreenActionHandler.initializeViews(profileImageButton, createButton);
+
+        HomeScreenActionHandler.handleProfileImageButtonClick(this);
+        HomeScreenActionHandler.handleCreateButtonClick(this);
+        HomeScreenActionHandler.handleLoadingTodoList(this, scrollLayout, eventAlert);
     }
 }
