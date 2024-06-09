07eaa8c3c107dbf848200f6f92c324252d2cbe9d
malakashashiranga <malakashashiranga115@gmail.com>
2024-06-10 00:21:07 +0530

    add new event screen actions

--- app/src/main/java/com/example/todomo/new_event_activity/NewEventScreenActivity.java
+++ app/src/main/java/com/example/todomo/new_event_activity/NewEventScreenActivity.java
@@ -2,8 +2,11 @@ package com.example.todomo.new_event_activity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ProgressBar;
 
+import androidx.activity.OnBackPressedCallback;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.todomo.R;
@@ -17,5 +20,22 @@ public class NewEventScreenActivity extends AppCompatActivity {
 
         ProgressBar progressBar = findViewById(R.id.EventSpinnerBar);
         progressBar.setVisibility(View.INVISIBLE);
+
+        Button saveButton = findViewById(R.id.SaveButton);
+
+        ImageButton backButton = findViewById(R.id.BackArrow);
+
+        NewEventScreenActionHandler.initializeViews(saveButton);
+
+        backButton.setOnClickListener(v -> NewEventScreenActionHandler.handleBackButton(this));
+        NewEventScreenActionHandler.handleSaveButtonClick(this, progressBar);
+
+        OnBackPressedCallback callback = new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                NewEventScreenActionHandler.handleBackButton(NewEventScreenActivity.this);
+            }
+        };
+        getOnBackPressedDispatcher().addCallback(this, callback);
     }
 }
