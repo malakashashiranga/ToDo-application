e7818825fb9444eef4f09729a06e1ea063af4ccc
malakashashiranga <malakashashiranga115@gmail.com>
2024-06-10 14:26:04 +0530

    add developer screen actions

--- app/src/main/java/com/example/todomo/developer_activity/DeveloperScreenActivity.java
+++ app/src/main/java/com/example/todomo/developer_activity/DeveloperScreenActivity.java
@@ -1,7 +1,9 @@
 package com.example.todomo.developer_activity;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
 
+import androidx.activity.OnBackPressedCallback;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.todomo.R;
@@ -12,5 +14,18 @@ public class DeveloperScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_developer_screen);
+
+
+        ImageButton backButton = findViewById(R.id.BackArrow);
+
+        backButton.setOnClickListener(v -> DeveloperScreenActionHandler.handleBackButtonClick(this));
+
+        OnBackPressedCallback callback = new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                DeveloperScreenActionHandler.handleBackButtonClick(DeveloperScreenActivity.this);
+            }
+        };
+        getOnBackPressedDispatcher().addCallback(this, callback);
     }
 }
