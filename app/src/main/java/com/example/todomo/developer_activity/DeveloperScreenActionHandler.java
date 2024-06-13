e11bbb06d5358b859dd5500561113320799a3162
malakashashiranga <malakashashiranga115@gmail.com>
2024-06-14 00:07:58 +0530

    create back button action

--- app/src/main/java/com/example/todomo/developer_activity/DeveloperScreenActionHandler.java
+++ app/src/main/java/com/example/todomo/developer_activity/DeveloperScreenActionHandler.java
@@ -6,11 +6,12 @@ import android.content.Intent;
 import androidx.annotation.NonNull;
 
 import com.example.todomo.R;
+import com.example.todomo.profile_activity.ProfileScreenActivity;
 
 public class DeveloperScreenActionHandler {
 
     public static void handleBackButtonClick(@NonNull Activity activity) {
-        Intent intent = new Intent(activity, DeveloperScreenActivity.class);
+        Intent intent = new Intent(activity, ProfileScreenActivity.class);
         activity.startActivity(intent);
         activity.overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
         activity.finish();
